4b2e095c19a9b18fa7e0ede89df4520987b29ae8
Harunur Rashid <142937596+harunur6729@users.noreply.github.com>
2025-03-10 16:30:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+ const cors = require('cors');
+
+ const app = express();
+  const port = process.env.PORT || 5000;
+
+//   MIDDLEWARE
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('This is Coffee store server running')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`coffee server is running ${port}`)
+})
